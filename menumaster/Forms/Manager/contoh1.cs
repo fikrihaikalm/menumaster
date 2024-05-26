886da3fba3f30de05a24c0cd7f57de34c2c5a01c
Muhammad Fikri Haikal <153433767+fikrihaikalm@users.noreply.github.com>
2024-05-27 03:53:42 +0700

    ubah struktur lagi

---
+++ menumaster/Forms/Manager/contoh1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace menumaster.Forms.Manager
+{
+    public partial class contoh1 : Form
+    {
+        public contoh1()
+        {
+            InitializeComponent();
+        }
+    }
+}
