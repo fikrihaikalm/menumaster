b5185dbe2e83c4fffc9021f970a4104e75bb7d3e
greek hold <greekhold1864@gmail.com>
2024-06-05 16:22:39 +0700

    login nyambung ke database

--- menumaster/Views/waiter_login.Designer.cs
+++ menumaster/Views/waiter_login.Designer.cs
@@ -30,7 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login_kasir));
             button1 = new Button();
-            textBox2 = new TextBox();
+            passwordtxt = new TextBox();
             label4 = new Label();
             textBox1 = new TextBox();
             label3 = new Label();
@@ -45,49 +45,56 @@
             // 
             button1.Anchor = AnchorStyles.None;
             button1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(194, 419);
+            button1.Location = new Point(315, 670);
+            button1.Margin = new Padding(5, 5, 5, 5);
             button1.Name = "button1";
-            button1.Size = new Size(102, 42);
+            button1.Size = new Size(166, 67);
             button1.TabIndex = 13;
             button1.Text = "Login";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
-            // textBox2
+            // passwordtxt
             // 
-            textBox2.Anchor = AnchorStyles.None;
-            textBox2.Location = new Point(59, 351);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(237, 27);
-            textBox2.TabIndex = 12;
+            passwordtxt.Anchor = AnchorStyles.None;
+            passwordtxt.Location = new Point(96, 562);
+            passwordtxt.Margin = new Padding(5, 5, 5, 5);
+            passwordtxt.Name = "passwordtxt";
+            passwordtxt.Size = new Size(383, 39);
+            passwordtxt.TabIndex = 12;
+            passwordtxt.TextChanged += passwordtxt_TextChanged;
             // 
             // label4
             // 
             label4.Anchor = AnchorStyles.None;
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(59, 310);
+            label4.Location = new Point(96, 496);
+            label4.Margin = new Padding(5, 0, 5, 0);
             label4.Name = "label4";
-            label4.Size = new Size(97, 28);
+            label4.Size = new Size(157, 45);
             label4.TabIndex = 11;
             label4.Text = "Password";
             // 
             // textBox1
             // 
             textBox1.Anchor = AnchorStyles.None;
-            textBox1.Location = new Point(59, 244);
+            textBox1.Location = new Point(96, 390);
+            textBox1.Margin = new Padding(5, 5, 5, 5);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(237, 27);
+            textBox1.Size = new Size(383, 39);
             textBox1.TabIndex = 10;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // label3
             // 
             label3.Anchor = AnchorStyles.None;
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(59, 213);
+            label3.Location = new Point(96, 341);
+            label3.Margin = new Padding(5, 0, 5, 0);
             label3.Name = "label3";
-            label3.Size = new Size(32, 28);
+            label3.Size = new Size(52, 45);
             label3.TabIndex = 9;
             label3.Text = "ID";
             // 
@@ -96,9 +103,10 @@
             label2.Anchor = AnchorStyles.None;
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(59, 148);
+            label2.Location = new Point(96, 237);
+            label2.Margin = new Padding(5, 0, 5, 0);
             label2.Name = "label2";
-            label2.Size = new Size(237, 25);
+            label2.Size = new Size(378, 40);
             label2.TabIndex = 8;
             label2.Text = "please login to your account";
             // 
@@ -107,9 +115,10 @@
             label1.Anchor = AnchorStyles.None;
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 37.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(41, 34);
+            label1.Location = new Point(67, 54);
+            label1.Margin = new Padding(5, 0, 5, 0);
             label1.Name = "label1";
-            label1.Size = new Size(508, 85);
+            label1.Size = new Size(811, 135);
             label1.TabIndex = 7;
             label1.Text = "Selamat Datang";
             // 
@@ -117,9 +126,10 @@
             // 
             button2.Anchor = AnchorStyles.None;
             button2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button2.Location = new Point(59, 419);
+            button2.Location = new Point(96, 670);
+            button2.Margin = new Padding(5, 5, 5, 5);
             button2.Name = "button2";
-            button2.Size = new Size(102, 42);
+            button2.Size = new Size(166, 67);
             button2.TabIndex = 14;
             button2.Text = "Back";
             button2.UseVisualStyleBackColor = true;
@@ -129,29 +139,31 @@
             // 
             pictureBox1.Anchor = AnchorStyles.None;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(555, 113);
+            pictureBox1.Location = new Point(902, 181);
+            pictureBox1.Margin = new Padding(5, 5, 5, 5);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(448, 348);
+            pictureBox1.Size = new Size(728, 557);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 15;
             pictureBox1.TabStop = false;
             // 
             // Login_kasir
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(1091, 640);
+            ClientSize = new Size(1773, 1024);
             Controls.Add(pictureBox1);
             Controls.Add(button2);
             Controls.Add(button1);
-            Controls.Add(textBox2);
+            Controls.Add(passwordtxt);
             Controls.Add(label4);
             Controls.Add(textBox1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            MinimumSize = new Size(800, 600);
+            Margin = new Padding(5, 5, 5, 5);
+            MinimumSize = new Size(1284, 917);
             Name = "Login_kasir";
             Text = "Login kasir";
             WindowState = FormWindowState.Maximized;
@@ -164,7 +176,7 @@
         #endregion
 
         private Button button1;
-        private TextBox textBox2;
+        private TextBox passwordtxt;
         private Label label4;
         private TextBox textBox1;
         private Label label3;
--- menumaster/Views/waiter_login.cs
+++ menumaster/Views/waiter_login.cs
@@ -1,13 +1,8 @@
 ﻿using menumaster.Views;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace menumaster
 {
@@ -32,11 +27,45 @@ namespace menumaster
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string connString = "Host=localhost;Username=postgres;Password=1;Database=menumaster";
+
+            // Update query to check role in addition to ID and password
+            string query = "SELECT * FROM karyawan WHERE id_karyawan = @id_karyawan AND password = @password and id_role = 4" ;
+            using (NpgsqlConnection conn = new NpgsqlConnection(connString))
             {
-                waiter_homepage homepage = new waiter_homepage();
-                homepage.Show();
-                this.Close();
+                conn.Open();
+
+                using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("id_karyawan", int.Parse(textBox1.Text));
+                    cmd.Parameters.AddWithValue("password", passwordtxt.Text);
+
+                    var result = cmd.ExecuteScalar() as int?;
+
+                    if (result.HasValue && result.Value > 0)
+                    {
+                        // ID, password valid dan role waiter
+                        waiter_homepage homepage = new waiter_homepage();
+                        homepage.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        // ID, password salah atau bukan role waiter
+                        MessageBox.Show("ID, password salah atau bukan role waiter", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
             }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void passwordtxt_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
