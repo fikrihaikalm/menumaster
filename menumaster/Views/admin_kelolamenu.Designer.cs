b462e436dccc44c9cbd1479c3f8d355706332334
laely dwi harianti <lilyanti33@gmail.com>
2024-06-06 21:32:20 +0700

    kelola menu fiks

--- menumaster/Views/admin_kelolamenu.Designer.cs
+++ menumaster/Views/admin_kelolamenu.Designer.cs
@@ -28,12 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "admin_kelolamenu";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(admin_kelolamenu));
+            button1 = new Button();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            groupBox1 = new GroupBox();
+            comboBox1 = new ComboBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.None;
+            button1.BackColor = Color.DarkSlateBlue;
+            button1.Font = new Font("Franklin Gothic Medium Cond", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.HighlightText;
+            button1.Location = new Point(20, 623);
+            button1.Name = "button1";
+            button1.Size = new Size(135, 54);
+            button1.TabIndex = 28;
+            button1.Text = "Back";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Font = new Font("Cooper Black", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(23, 9);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(204, 35);
+            label1.TabIndex = 30;
+            label1.Text = "Kelola Menu";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.None;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(479, 162);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(487, 412);
+            dataGridView1.TabIndex = 31;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(160, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(182, 31);
+            textBox1.TabIndex = 32;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(160, 128);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(182, 31);
+            textBox2.TabIndex = 33;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(25, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(113, 25);
+            label2.TabIndex = 35;
+            label2.Text = "Nama          :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(25, 131);
+            label3.Name = "label3";
+            label3.Size = new Size(114, 25);
+            label3.TabIndex = 36;
+            label3.Text = "Harga          :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(24, 219);
+            label4.Name = "label4";
+            label4.Size = new Size(113, 25);
+            label4.TabIndex = 37;
+            label4.Text = "Deskripsi     :";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Anchor = AnchorStyles.None;
+            groupBox1.BackColor = SystemColors.GradientInactiveCaption;
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(33, 162);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(396, 366);
+            groupBox1.TabIndex = 38;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Tambah Menu";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Makanan", "Minuman", "Snack" });
+            comboBox1.Location = new Point(160, 296);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 39;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(25, 299);
+            label5.Name = "label5";
+            label5.Size = new Size(112, 25);
+            label5.TabIndex = 38;
+            label5.Text = "Kategori      :";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(160, 184);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(182, 91);
+            textBox3.TabIndex = 34;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.None;
+            pictureBox1.BackgroundImage = Properties.Resources.ep_back;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(33, 639);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(26, 24);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 40;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.None;
+            button2.BackColor = Color.MistyRose;
+            button2.Location = new Point(104, 540);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 40);
+            button2.TabIndex = 41;
+            button2.Text = "Hapus";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.None;
+            button3.BackColor = SystemColors.GradientActiveCaption;
+            button3.Location = new Point(263, 540);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 40);
+            button3.TabIndex = 42;
+            button3.Text = "Simpan";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // admin_kelolamenu
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(978, 744);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(pictureBox1);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "admin_kelolamenu";
+            Text = "admin_kelolamenu";
+            WindowState = FormWindowState.Maximized;
+            Load += admin_kelolamenu_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Button button1;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private GroupBox groupBox1;
+        private ComboBox comboBox1;
+        private Label label5;
+        private TextBox textBox3;
+        private PictureBox pictureBox1;
+        private Button button2;
+        private Button button3;
     }
 }
--- menumaster/Views/admin_kelolamenu.cs
+++ menumaster/Views/admin_kelolamenu.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace menumaster.Views
 {
@@ -15,6 +16,123 @@ namespace menumaster.Views
         public admin_kelolamenu()
         {
             InitializeComponent();
+            LoadData();
+            PopulateComboBox();
+        }
+
+        private DataTable kelolamenu()
+        {
+            string connectionString = "Host=localhost;Username=postgres;Password=1;Database=menu master";
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                string query = "SELECT id_kategori, kategori FROM kategori_menu";
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
+        }
+
+        private void PopulateComboBox()
+        {
+            DataTable categories = kelolamenu();
+            comboBox1.DataSource = categories;
+            comboBox1.DisplayMember = "kategori";
+            comboBox1.ValueMember = "id_kategori";
+        }
+
+        private void admin_kelolamenu_Load(object sender, EventArgs e)
+        {
+            PopulateComboBox();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            admin_page admin_page = new admin_page();
+            admin_page.Show();
+            this.Hide();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadData()
+        {
+            string connectionString = "Host=localhost;Username=postgres;Password=1;Database=menu master";
+            NpgsqlConnection conn = new NpgsqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+                string sql = "SELECT id_menu, nama AS menu, harga, deskripsi FROM menu";
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string namaMenu = textBox1.Text;
+            decimal hargaMenu;
+            if (!decimal.TryParse(textBox2.Text, out hargaMenu))
+            {
+                MessageBox.Show("Harga harus berupa angka");
+                return;
+            }
+            string deskripsiMenu = textBox3.Text;
+            int jenisKategori = (int)comboBox1.SelectedValue;
+
+            string connectionString = "Host=localhost;Username=postgres;Password=1;Database=menu master";
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                string masuk = "INSERT INTO menu (nama, harga, deskripsi, id_kategori) VALUES (@nama, @harga, @deskripsi, @id_kategori)";
+                using (NpgsqlCommand command = new NpgsqlCommand(masuk, connection))
+                {
+                    command.Parameters.AddWithValue("nama", namaMenu);
+                    command.Parameters.AddWithValue("harga", hargaMenu);
+                    command.Parameters.AddWithValue("deskripsi", deskripsiMenu);
+                    command.Parameters.AddWithValue("id_kategori", jenisKategori);
+
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    connection.Close();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Berhasil!");
+                        textBox1.Clear();
+                        textBox2.Clear();
+                        textBox3.Clear();
+                        comboBox1.SelectedIndex = -1;
+
+                        LoadData();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Gagal");
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            comboBox1.SelectedIndex = -1;
         }
     }
 }
