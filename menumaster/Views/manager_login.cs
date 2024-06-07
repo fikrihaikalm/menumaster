b062506aa055894c2c3c2be9c35c2dd29369938a
laely dwi harianti <lilyanti33@gmail.com>
2024-06-07 10:26:57 +0700

    login manager

--- menumaster/Views/manager_login.cs
+++ menumaster/Views/manager_login.cs
@@ -8,11 +8,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace menumaster
 {
     public partial class Login_manajer : Form
     {
+        public static int KaryawanID { get; private set; }
+
         public Login_manajer()
         {
             InitializeComponent();
@@ -20,10 +23,62 @@ namespace menumaster
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Manager_Homepage f1 = new Manager_Homepage();
-            f1.Show();
-            this.Hide();
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("ID dan password tidak boleh kosong", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int idKaryawan;
+            if (!int.TryParse(textBox1.Text, out idKaryawan))
+            {
+                MessageBox.Show("ID karyawan harus berupa angka", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string query = "SELECT COUNT(*) FROM karyawan WHERE id_karyawan = @id_karyawan AND password = @password AND id_role = 1";
+
+            NpgsqlParameter[] parameters = {
+                new NpgsqlParameter("@id_karyawan", idKaryawan),
+                new NpgsqlParameter("@password", textBox2.Text)
+            };
+
+            string connectionString = "Host=localhost;Username=postgres;Password=1;Database=menu master";
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
+                    {
+                        command.Parameters.AddRange(parameters);
+                        int result = Convert.ToInt32(command.ExecuteScalar());
+
+                        if (result > 0)
+                        {
+                            // Simpan ID karyawan
+                            KaryawanID = idKaryawan;
+
+                            // ID, password valid dan role manager
+                            Manager_Homepage homepage = new Manager_Homepage();
+                            homepage.Show();
+                            this.Close();
+                        }
+                        else
+                        {
+                            // ID, password salah atau bukan role manager
+                            MessageBox.Show("ID, password salah atau bukan role manager", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
+
         private void button2_Click(object sender, EventArgs e)
         {
             WelcomePageForm f1 = new WelcomePageForm();
